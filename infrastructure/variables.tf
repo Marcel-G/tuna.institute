85ed7d01f58e6f29ae212497936926720aa3e744
Marcel Gleeson <gleeson.marcel@gmail.com>
2022-06-27 21:09:41 +0200

    Add terraform

---
+++ infrastructure/variables.tf
@@ -0,0 +1,26 @@
+variable "website-domain-main" {
+  description = "Main website domain, e.g. cloudmaniac.net"
+  type        = string
+}
+
+variable "website-domain-redirect" {
+  description = "Secondary FQDN that will redirect to the main URL, e.g. www.cloudmaniac.net"
+  default     = null
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags added to resources"
+  default     = {}
+  type        = map(string)
+}
+
+variable "org" {
+  description = "Github organisation name"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "Github repo name"
+  type        = string
+}
